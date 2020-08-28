fba139c21894ee283acb37f688f4e56c575ecf05
msjones3 <51344555+msjones3@users.noreply.github.com>
2020-08-29 05:00:15 +1000

    Add files via upload

---
+++ image-converter.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image
+''' Click on Import JPG File then select image.
+Click on Convert to change it to a png. Enter a new filename. 
+This program was written by Data to Fish https://datatofish.com/jpeg-to-png-python/'''
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 250, bg = 'lightsteelblue2', relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Convert JPG to PNG', bg = 'lightsteelblue2')
+label1.config(font=('helvetica', 20))
+canvas1.create_window(150, 60, window=label1)
+
+def getJPG ():
+    global im1
+    
+    import_file_path = filedialog.askopenfilename()
+    im1 = Image.open(import_file_path)
+    
+browseButton_JPG = tk.Button(text="      Import JPG File     ", command=getJPG, bg='royalblue', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 130, window=browseButton_JPG)
+
+def convertToPNG ():
+    global im1
+    
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.png')
+    im1.save(export_file_path)
+
+saveAsButton_PNG = tk.Button(text='Convert', command=convertToPNG, bg='royalblue', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 180, window=saveAsButton_PNG)
+
+
+root.mainloop()
